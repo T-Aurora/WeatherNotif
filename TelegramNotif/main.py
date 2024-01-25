f9bf5d4a2de294470acd327deb344c9844799a2a
T-Aurora <auror.tll@gmail.com>
2024-01-25 14:14:23 +0100

    Primo commit

--- TelegramNotif/main.py
+++ TelegramNotif/main.py
@@ -47,22 +47,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    """
-    p = requests.get('http://127.0.0.1:5000/start_producer')
-    print("Kafka consumer running in the background...")
-    application = ApplicationBuilder().token(token).build()
-    start_handler = CommandHandler('start', commands.start)
-    #echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), commands.echo)
-    sub_handler = CommandHandler('sub',commands.sub)
-    help_handler = CommandHandler('help', commands.help)
-    alert_handler = CommandHandler('alert', commands.alert)
-    unknown_handler = MessageHandler(filters.COMMAND, commands.unknown)
-    application.add_handler(start_handler)
-    application.add_handler(sub_handler)
-    #application.add_handler(echo_handler)
-    application.add_handler(help_handler)
-    application.add_handler(alert_handler)
-    application.add_handler(unknown_handler)
-    application.run_polling()
-
-"""
